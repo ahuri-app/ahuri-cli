60072b1d0d93cb21224144c0184c2fd96af61c11
SqdNoises <sqdnoises@gmail.com>
2022-11-24 16:07:12 +0530

    new version 1.0.1

--- ahuri/__init__.py
+++ ahuri/__init__.py
@@ -8,5 +8,5 @@ __full_title__ = "Ahuri CLI"
 __author__ = "SqdNoises"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022-present SqdNoises"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __display_version__ = "ahuri-cli " + __version__
